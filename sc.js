97c55ed9439347f573c6d98f6b1bee97a2719ab4
Ravi Aajugiya <raviaajugiya@weybee.in>
2023-09-04 18:30:00 +0530

    Changes to be committed: 	modified:   Gallery.html 	new file:   Images/team.jpg 	modified:   about.css 	modified:   about.css.map 	modified:   about.html 	modified:   about.scss 	modified:   contact.html 	modified:   index.css 	modified:   index.css.map 	modified:   index.html 	modified:   index.scss 	modified:   main.css 	modified:   main.css.map 	modified:   main.scss 	deleted:    product-details.html 	new file:   productdetails.html 	new file:   sc.js 	new file:   script.js 	new file:   temp/index.html 	new file:   temp/style.css 	new file:   temp/style.css.map 	new file:   temp/style.scss 	modified:   update.html

---
+++ sc.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".dropdown").click(function () {
+        $(".dropdown-menu").toggleClass("show")
+    })
+});
+$(document).ready(function () {
+    $(".navbar-toggler").click(function () {
+        $(".navbar-collapse").toggleClass("show")
+    })
+})
