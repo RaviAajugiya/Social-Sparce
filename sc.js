aae75461f60b9089086fe4cd809cd87e0198d537
Ravi Aajugiya <raviaajugiya@weybee.in>
2023-09-05 18:46:43 +0530

    modified:   index.css.map 	modified:   index.html 	modified:   index.scss 	modified:   sc.js 	modified:   update.html

--- sc.js
+++ sc.js
@@ -1,28 +1,38 @@
 
+function clickHandle(e) {
+    console.log(e.target.className);
+    if(!(e.target.className == 'navbar-nav')){
+        document.querySelector(".navbar-collapse").classList.remove("show")
+    }
+    if((e.target.className == 'modal')){
+        document.querySelector(".modal").hidden = true;
+    }
+    document.querySelector(".dropdown-menu").classList.remove("show")
 
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector(".dropdown").addEventListener('click', () => {
-        document.querySelector(".dropdown-menu").classList.toggle("show")
+}
+
+window.addEventListener('mouseup', clickHandle)
+
+document.querySelector(".dropdown").addEventListener('click', () => {
+    document.querySelector(".dropdown-menu").classList.toggle("show")
+})
+
+document.querySelector(".navbar-toggler").addEventListener('click', () => {
+    document.querySelector(".navbar-collapse").classList.toggle("show")
+
+})
+
+document.querySelectorAll(".share-btn").forEach((c) => {
+
+    c.addEventListener('click', () => {
+        document.querySelector(".modal").hidden = false;
     })
+})
 
-    document.querySelector(".navbar-toggler").addEventListener('click', () => {
-        document.querySelector(".navbar-collapse").classList.toggle("show")
+document.querySelectorAll(".close").forEach((c) => {
+
+    c.addEventListener('click', () => {
+        document.querySelector(".modal").hidden = true;
     })
-
-    document.querySelectorAll(".share-btn").forEach((c) => {
-        c.addEventListener('click', () => {
-            document.querySelector(".modal").hidden = false;
-        })
-    })
-
-    document.querySelectorAll(".close").forEach((c) => {
-        c.addEventListener('click', () => {
-            document.querySelector(".modal").hidden = true;
-        })
-    })
-
-
-
-
-});
+})
 
